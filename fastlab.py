35e5b9bcffecd906ba81bac13c84b6e8580e2c4e
Dmitriy Maslennikov <dy.maslennikov@gmail.com>
2023-12-08 05:38:43 +0300

    Change 5 add test 2+2=?

--- fastlab.py
+++ fastlab.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
 app = FastAPI()
-# Hello World route
+def sum_two_args(x, y):
+  return x + y
 @app.get("/")
 def read_root():
   return {"Hello": "World"}
 if __name__ == "__main__":
-  uvicorn.run(app, host= "127.0.0.1", port=8000)
+  uvicorn.run(app, host= "0.0.0.0", port=8000)
